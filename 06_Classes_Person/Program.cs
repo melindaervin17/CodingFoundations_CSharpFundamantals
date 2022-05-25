222dd75b9541c11a6da3ef38e1494857b4f0d45f
Melinda Ervin <melindaervin17@gmail.com>
2022-05-24 20:44:25 -0400

    06_Classes-Person Notes

---
+++ 06_Classes_Person/Program.cs
@@ -0,0 +1,91 @@
+﻿//! Person Class
+
+Person person = new Person();
+person.LastName = "Parker";
+person.FirstName = "Peter";
+person.DateOfBirth = new DateTime(1962, 6, 5);
+System.Console.WriteLine($"{person.FullName} is {person.Age} years old.");
+
+Person person2 = new Person("Bruce", "Wayne", new DateTime(1939, 5, 1));
+System.Console.WriteLine($"{person2.FullName} is {person2.Age} years old.");
+
+Person person3 = new Person(1963, "Charles", "Xavier");
+System.Console.WriteLine(person3.ID);
+// person3.ID = 10;
+// System.Console.WriteLine(person3.ID);
+
+//!--------------------------------------CLASS OBJECT--------------------------------------------------
+public class Person {
+
+	//PROPERTIES
+	public string FirstName { get; set; }
+
+	//Field
+	private string _lastName; // fields are private
+
+	public string LastName {
+		/*
+			- We have a method to get the person latname and set the person last name
+			- We can customize how our get/set are detailed
+			- Setting just a "get; set;" is just shorthand for what this property is doing
+		*/
+
+			get { return _lastName; }
+			set {_lastName = value; } // value is a keyword specfic to this situation and equals whatever value was provided when creating the person
+	}
+
+	public string FullName { 
+		get { return $"{FirstName} {LastName}";}
+	}
+
+	public DateTime DateOfBirth { get; set; }
+
+	public int Age { 
+		get {
+			TimeSpan exactAge = DateTime.Now - DateOfBirth;
+			double totalDays = exactAge.TotalDays;
+			double yearsUnrounded = totalDays / 365.24;
+			int years = (int) Math.Round(yearsUnrounded); // Math will provide us a double datetime, so we will cast it as an int that Rounds up.
+			return years;
+		}
+	}
+
+	//! Constructors
+	/*
+		- Invoked automaticlaly when object is created
+		- Initializes the data members of a new object
+
+		Has two tyoes:
+			- Default constructor
+			- Parameterized constructor
+	*/
+
+	public Person(){} // We have cfreated a b;ank constructor (default)
+	//* ctor - emmet for constructor buoilerplate
+
+	public Person(string firstName, string lastName, DateTime dateOfBirth)
+	{
+	//		1			2	
+		FirstName = firstName;
+		/*
+			1. Property name
+			2. value to be set to property
+		*/
+		_lastName = lastName;
+		DateOfBirth = dateOfBirth;
+	}
+
+	public Person(int id, string firstName, string lastName)
+	{
+		_idNumber = id;
+		FirstName = firstName;
+		_lastName = lastName;
+	}
+
+	private int _idNumber;
+	public int ID
+	{
+		get { return _idNumber;}
+		// When we assign the iD of the person up creation (in the 3rd constructor), we cannot alter it because it is a Read Only.
+	}
+}
---
+++ 06_Classes_Vehicle/Program.cs
@@ -0,0 +1,64 @@
+﻿//! Classes
+/*
+	- Custom objects in C#
+	- Have properties and methods
+		- Properties: variables that exist in the entire class
+
+	Keywords:
+		- public: can be accessed by any other code in the same assembly or another assembly that refernece it
+		- private: can only be accessed in the same class or struct
+		- protected: can be accessed only by code in the same class, or in a class that is derived from that class
+		- internal: type or member can be accessed by any code in the same assembly, but not from another assembly
+		- protected internal: can be accessed by any code in the assembly in which it's declared, or from within a derived class in another assembly
+		- private protected: can be accessed only within its declaring assembly, by code in the same class or in a type that is derived from that class
+
+	Structs:
+		- used from small data-centric types with little or no behavior
+*/
+
+Vehicle vehicle = new Vehicle();
+vehicle.Model = "Journey";
+System.Console.WriteLine($"I drive a {vehicle.Model}. Its make is {vehicle.Make}.");
+vehicle.Make = "Dodge";
+vehicle.Mileage = 7500;
+System.Console.WriteLine($"I drive a {vehicle.Make} with {vehicle.Mileage} miles on it.");
+vehicle.VehicleType = VehicleType.Car;
+
+/*
+! Enums
+	Each tem is an enum needs to ahve a unique index value. The default value will always be zero
+*/
+public enum VehicleType {
+	Car = 1, // The index defaults to zero; however we can set our index as such
+	Truck,
+	Van,
+	Motorcycle,
+	Spaceship,
+	Plane,
+	Boat
+}
+
+public class Vehicle { // We have created this new data type that we can now use
+
+	/*
+	! Properties
+			set: (setters)
+				- allows us to set attributes to the object we are creating
+				- can ahve a default value
+			get: (getters)
+				- allows us to view (get) a value of a property within our object
+
+			Can be set a nul value
+	*/
+
+	public string Make {get; set;} = "Unknown Manufacturer"; // setting a default value to our property "Make"
+	//* prop - emmet for creating a property
+
+	public string Model { get; set; }	
+
+	public double? Mileage { get; set; } // placing a ? denotes that it may be a null value
+
+	public int Year { get; set; }
+	public VehicleType VehicleType { get; set; } // our enum propperty
+
+}
